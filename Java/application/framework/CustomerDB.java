668a0629544819e733df3519a32aef2c0568f6e6
Truong Thanh Nam Nguyen <namco2011@gmail.com>
2020-11-17 23:51:01 -0600

    update UI

--- Java/application/framework/CustomerDB.java
+++ Java/application/framework/CustomerDB.java
@@ -10,8 +10,8 @@ public class CustomerDB {
 
     //String AccountNumber, String name, String emailAddress, String street, String city, String state, String zip
     static{
-        Customer customerAccount1 = new Customer("11111111", "Jhon", "jhon@gmail.com", "101 street", "Fairfield", "Iowa", "52556");
-        Customer customerAccount2 = new Customer("22222222", "Frank", "frank@gmail.com", "102 street", "Fairfield", "Iowa", "52556");
+        Customer customerAccount1 = new Customer( "Jhon", "jhon@gmail.com", "101 street", "Fairfield", "Iowa", "52556");
+        Customer customerAccount2 = new Customer("Frank", "frank@gmail.com", "102 street", "Fairfield", "Iowa", "52556");
 
         customerList.add(customerAccount1);
         customerList.add(customerAccount2);
